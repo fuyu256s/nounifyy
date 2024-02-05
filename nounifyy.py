a5154185aeff659521ceba5bc1797648e80dde4e
fuyu256 <106586954+fuyu256s@users.noreply.github.com>
2024-02-05 19:24:00 +0900

    add nounifyy gif

--- nounifyy.py
+++ nounifyy.py
@@ -8,11 +8,12 @@ import numpy as np
 import requests
 import streamlit as st
 from PIL import Image
-from utils import st_dl_png
+from stqdm import stqdm
+from utils import st_dl_gif, st_dl_png
 
 
 def main() -> None:
-    file = st.file_uploader("Upload a file", type=['png', 'jpg'])
+    file = st.file_uploader("Upload a file", type=['png', 'jpg', 'gif'])
     url = st.text_input("or enter a URL")
 
     if url:
@@ -21,10 +22,15 @@ def main() -> None:
         file.name = ""
 
     if file is not None:
-        im = nounifyy(file)
-        st.image(im)
-        stem = "" if url else "".join(file.name.split(".")[:-1])
-        st_dl_png(im, "nounified_" + stem, 'dl_sv2png')
+        im = Image.open(file)
+        if im.format == 'GIF':
+            ims, durations = nounifyy_gif(im)
+            st_dl_gif(ims, durations, "output", 'dl_nnfgif')
+        else:
+            im = nounifyy(im)
+            st.image(im)
+            stem = "" if url else "".join(file.name.split(".")[:-1])
+            st_dl_png(im, "nounified_" + stem, 'dl_nnf')
     else:
         st.divider()
         st.write("Go to other tools:")
@@ -37,13 +43,13 @@ def main() -> None:
         st.write("<- check the sidebar for page switching and tool controls")
 
 
-def nounifyy(file: IO | Path) -> Image:
+def nounifyy(im: Image) -> Image:
     d_noggles = {
         x.stem.replace("glasses-", "").replace("square-", ""): x
         for x in Path("4-glasses").glob("*.png")
     }
 
-    arr = np.array(_reduce_im_size(Image.open(file).convert('RGB')))
+    arr = np.array(_reduce_im_size(im.convert('RGB')))
     face_landmarks_list = face_recognition.face_landmarks(arr)
     n = len(face_landmarks_list)
 
@@ -137,6 +143,71 @@ def _get_noggle_place(
     noggle = noggle.resize(
         (np.array(noggle.size) * factor).astype(int), Image.NEAREST
     ).rotate(st_angle, Image.BICUBIC, center=tuple(center.astype(int)))
+
+    return place, noggle
+
+
+def nounifyy_gif(im: Image) -> Image:
+    d_noggles = {
+        x.stem.replace("glasses-", "").replace("square-", ""): x
+        for x in Path("4-glasses").glob("*.png")
+    }
+    d = {"random": random.choice(list(d_noggles.values()))} | d_noggles
+    st_type = st.sidebar.selectbox("type", d.keys(), key=f"type_gif")
+    p_noggles = d[st_type]
+
+    flag, durations, ims = [], [], []
+    for i in stqdm(range(im.n_frames)):
+        im.seek(i)
+        durations.append(im.info['duration'])
+
+        arr = np.array(_reduce_im_size(im.convert('RGB')))
+        face_landmarks_list = face_recognition.face_landmarks(arr)
+
+        init = {
+            'size': 1.25,
+            'x': 0.0,
+            'y': 0.0,
+        }
+
+        if face_landmarks_list:
+            newim = Image.fromarray(arr)
+            for i, face_landmarks in enumerate(face_landmarks_list):
+                place, noggle = _get_noggle_place_gif(
+                    i, face_landmarks, p_noggles, init
+                )
+                if place:
+                    newim.paste(noggle, box=(place), mask=noggle)
+            flag.append(True)
+        else:
+            newim = Image.fromarray(arr)
+            flag.append(False)
+        ims.append(newim.convert('P'))
+
+    st.warning(f"Face recognized: {sum(flag)}/{im.n_frames} frames")
+    return ims, durations
+
+
+def _get_noggle_place_gif(
+    i: int, face_landmarks: dict, p_noggles: str, init: dict
+) -> (tuple[int, int], Image):
+
+    le = np.mean(np.array(face_landmarks['left_eye']), axis=0).astype(int)
+    re = np.mean(np.array(face_landmarks['right_eye']), axis=0).astype(int)
+    v = le - re
+    angle = np.degrees(np.arctan2(v[0], v[1])) + 90
+    x, y, size = init['x'], init['y'], init['size']
+
+    factor = np.linalg.norm(v) / 7 * size
+    if factor.astype(int) == 0:
+        return None, None
+    center = (np.array((16.5, 13.5)) - np.array((x, y)) * 3) * factor
+    place = tuple((np.mean([le, re], axis=0) - center).astype(int))
+
+    noggle = Image.open(p_noggles)
+    noggle = noggle.resize(
+        (np.array(noggle.size) * factor).astype(int), Image.NEAREST
+    ).rotate(angle, Image.BICUBIC, center=tuple(center.astype(int)))
 
     return place, noggle
 
